180ed0011658786da98df47b131febc802edde3e
AbdouBenHatttab <abderrahmen.benhattab@bizerte.r-iset.tn>
2025-11-25 16:22:03 +0100

    v0 still testing merge in frount

--- HealthyfiTN_mobile/app/src/main/java/com/health/virtualdoctor/ui/data/api/RetrofitClient.kt
+++ HealthyfiTN_mobile/app/src/main/java/com/health/virtualdoctor/ui/data/api/RetrofitClient.kt
@@ -11,10 +11,10 @@ import java.util.concurrent.TimeUnit
 object RetrofitClient {
 
     // ✅ URLs des services via Cloudflare Tunnels
-    private const val AUTH_BASE_URL = "https://current-burning-edgar-bringing.trycloudflare.com" // Port 8082
-    private const val DOCTOR_BASE_URL = "https://sur-precious-controls-compounds.trycloudflare.com" // Port 8083
+    private const val AUTH_BASE_URL = "https://society-modem-seasons-locally.trycloudflare.com" // Port 8082
+    private const val DOCTOR_BASE_URL = "https://fighting-rebate-equilibrium-concentrate.trycloudflare.com" // Port 8083
     private const val NOTIFICATION_BASE_URL = "https://rank-construct-acquisitions-advertisers.trycloudflare.com" // Port 8084
-    private const val USER_BASE_URL = "https://accomplished-wishes-msie-gzip.trycloudflare.com" // Port 8085
+    private const val USER_BASE_URL = "https://quantity-likely-portions-locked.trycloudflare.com" // Port 8085
 
     private var authRetrofit: Retrofit? = null
     private var doctorRetrofit: Retrofit? = null
